ec840b5c4c40ae8a20ac144827f69a1bfbb68042
• 𝐒 𝚰 𝚲 • 💗 / ‹𝟹 🌷 <siyatechbot@gmail.com>
2025-12-15 12:07:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,13 +78,13 @@ STICKERS = [
     "CAACAgUAAx0Cd6nKUAACASBl_rnalOle6g7qS-ry-aZ1ZpVEnwACgg8AAizLEFfI5wfykoCR4h4E",
     "CAACAgUAAx0Cd6nKUAACATJl_rsEJOsaaPSYGhU7bo7iEwL8AAPMDgACu2PYV8Vb8aT4_HUPHgQ",
 ]
-HELP_IMG_URL = "https://files.catbox.moe/yg2vky.jpg"
+HELP_IMG_URL = "https://ar-hosting.pages.dev/1765780612901.jpg"
 PING_VID_URL = "https://ar-hosting.pages.dev/1765780495640.mp4"
-PLAYLIST_IMG_URL = "https://files.catbox.moe/yhaja5.jpg"
+PLAYLIST_IMG_URL = "https://ar-hosting.pages.dev/1765780612901.jpg"
 STATS_VID_URL = "https://ar-hosting.pages.dev/1765780495640.mp4"
 TELEGRAM_AUDIO_URL = "https://files.catbox.moe/mlztag.jpg"
 TELEGRAM_VIDEO_URL = "https://files.catbox.moe/tiss2b.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/1d3da7.jpg"
+STREAM_IMG_URL = "https://ar-hosting.pages.dev/1765780612901.jpg"
 SOUNCLOUD_IMG_URL = "https://files.catbox.moe/zhymxl.jpg"
 YOUTUBE_IMG_URL = "https://files.catbox.moe/veykzq.jpg"
 SPOTIFY_ARTIST_IMG_URL = SPOTIFY_ALBUM_IMG_URL = SPOTIFY_PLAYLIST_IMG_URL = YOUTUBE_IMG_URL
